bb7e098d9c278c7064f02d53d7a2717eaa5e4422
Sigwar <krukowski.rafal13@gmail.com>
2017-04-12 21:35:23 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app" class="ab">
+    <img class="abc" src="./assets/logo.png">
+    <div class="a">asdasdasd</div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+@import "../static/style/style.scss";
+</style>
