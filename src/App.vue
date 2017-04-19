cfa6abe0b48627464dc0a9c4829e5a75f6461a50
Sigwar <krukowski.rafal13@gmail.com>
2017-04-19 21:55:43 +0200

    Home section

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
-  <div id="app" class="ab">
-    <img class="abc" src="./assets/logo.png">
-    <div class="a">asdasdasd</div>
+  <div id="app" class="app">
+    <header class="header">
+      <nav class="header_navbar">
+        <img class="header_navbar--logo" src="~@/assets/logo.png">
+        <ul class="header_navbar--links">
+          <li class="link"><a class="link--href" href="#">Strona Główna</a></li>
+          <li class="link"><a class="link--href" href="#">O nas</a></li>
+          <li class="link"><a class="link--href" href="#">Plany</a></li>
+          <li class="link"><a class="link--href" href="#">Zborjownia</a></li>
+          <li class="link"><a class="link--href" href="#">Galeria</a></li>
+        </ul>
+      </nav>
+      <div class="header_title">
+        <h1 class="header_title--name">KRASJA</h1>
+        <hr class="header_title--line">
+        <h2 class="header_title--slogan"> Daj się porwać w wir przygody!</h2>
+      </div>
+    </header>
+    <home></home>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import Home from './views/homeView.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      Home
+    }
+  }
 </script>
 
 <style lang="scss">
-@import "../static/style/style.scss";
+  @import "../static/style/style.scss";
 </style>
---
+++ src/views/homeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style lang="scss">
+@improt '../../static/style/style.scss'
+</style>
